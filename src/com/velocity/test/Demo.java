295db9e63488e5cdfa28867d828baaabcfabc259
suraj_gotad <surajgotad111@gmail.com>
2022-12-24 20:03:34 +0530

    first comment : Array code

---
+++ src/com/velocity/test/Demo.java
@@ -0,0 +1,15 @@
+package com.velocity.test;
+
+import java.util.ArrayList;
+
+public class Demo {
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("suraj");
+		list.add("dipak");
+		list.add("sagar");
+		
+		System.out.println(list);
+	}
+
+}
