3db7bfa670d6243dc761e1b8e86ae68a22115511
suraj_gotad <surajgotad111@gmail.com>
2022-12-24 20:08:55 +0530

    second comment : LinkedList code

---
+++ src/com/velocity/test/Test.java
@@ -0,0 +1,16 @@
+package com.velocity.test;
+
+import java.util.LinkedList;
+
+public class Test {
+	public static void main(String[] args) {
+		LinkedList<Integer> ll=new LinkedList<Integer>();
+		ll.add(12);
+		ll.add(15);
+		ll.add(20);
+		
+		System.out.println(ll);
+		
+	}
+
+}
